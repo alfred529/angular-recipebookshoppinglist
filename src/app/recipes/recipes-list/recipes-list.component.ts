7cce8187994a8fe2c3d923cd6bfeef9160ee21b7
Alfred Law <41144911+alfred529@users.noreply.github.com>
2020-03-31 16:29:39 -0400

    Set up recipe model, dummy data, and generate list of recipes

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c0.wallpaperflare.com/preview/151/724/894/food-cooking-kitchen-cook.jpg')
+  ];
 
   constructor() { }
 
